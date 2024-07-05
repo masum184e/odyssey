d802f7f99afb6016a3147410b5892a01df9de992
masum184e <masum184e@gmail.com>
2024-07-06 05:09:31 +0600

    availability calender added

---
+++ app/src/main/java/com/example/odyssey/AvailabilityCalender.java
@@ -0,0 +1,12 @@
+package com.example.odyssey;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AvailabilityCalender  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.availability_calender);
+    }
+}
