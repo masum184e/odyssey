2fde43e7620ef0fcdee024d2c21c87307d61290a
masum184e <masum184e@gmail.com>
2024-07-06 04:40:54 +0600

    navigation added

---
+++ app/src/main/java/com/example/odyssey/BookingDetails.java
@@ -0,0 +1,13 @@
+package com.example.odyssey;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookingDetails  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.booking_details);
+
+    }
+}
--- app/src/main/java/com/example/odyssey/MainActivity.java
+++ app/src/main/java/com/example/odyssey/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.odyssey;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.booking_details);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.getStarted);
+        myButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.getStarted) {
+            Intent intent = new Intent(this, BookingDetails.class);
+            startActivity(intent);
+        }
     }
 }
