02e334489d0046ac70e2b3a7f7f21f3355f893ff
masum184e <masum184e@gmail.com>
2024-07-06 21:59:51 +0600

    tab content added

--- app/src/main/java/com/example/odyssey/BookingDetails.java
+++ app/src/main/java/com/example/odyssey/BookingDetails.java
@@ -3,6 +3,7 @@ package com.example.odyssey;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -59,6 +60,14 @@ public class BookingDetails extends AppCompatActivity implements View.OnClickLis
 
             imageContainer.addView(horizontalImage);
         }
+
+        Button reviewBtn=findViewById(R.id.reviewBtn);
+        Button aboutBtn=findViewById(R.id.aboutBtn);
+        LinearLayout carDetailsTabContainer = findViewById(R.id.carDetailsTabContainer);
+        View aboutCarView = getLayoutInflater().inflate(R.layout.about_car, carDetailsTabContainer, false);
+        carDetailsTabContainer.addView(aboutCarView);
+        reviewBtn.setOnClickListener(this);
+        aboutBtn.setOnClickListener(this);
     }
 
     @Override
@@ -66,6 +75,16 @@ public class BookingDetails extends AppCompatActivity implements View.OnClickLis
         if (view.getId() == R.id.headerBackBtn) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        } else if (view.getId() == R.id.reviewBtn) {
+            updateCarDetailsContent(R.layout.car_review);
+        } else if (view.getId() == R.id.aboutBtn) {
+            updateCarDetailsContent(R.layout.about_car);
         }
+    }
+    private void updateCarDetailsContent(int layoutId) {
+        LinearLayout carDetailsTabContainer = findViewById(R.id.carDetailsTabContainer);
+        carDetailsTabContainer.removeAllViews();
+        View aboutCarView = getLayoutInflater().inflate(layoutId, carDetailsTabContainer, false);
+        carDetailsTabContainer.addView(aboutCarView);
     }
 }
