354b142f663c7970ab548104d5432a6a4ea8738a
masum184e <masum184e@gmail.com>
2024-07-07 12:51:33 +0600

    back btn handled

--- app/src/main/java/com/example/odyssey/UploadVehicle.java
+++ app/src/main/java/com/example/odyssey/UploadVehicle.java
@@ -1,12 +1,24 @@
 package com.example.odyssey;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UploadVehicle  extends AppCompatActivity {
+public class UploadVehicle  extends AppCompatActivity implements  View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload_vehicle);
+
+        ImageView headerBackBtn=findViewById(R.id.headerBackBtn);
+        headerBackBtn.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        if (view.getId() == R.id.headerBackBtn) {
+            Intent intent = new Intent(this, BookingDetails.class);
+            startActivity(intent);
+        }
     }
 }
